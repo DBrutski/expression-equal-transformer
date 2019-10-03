1b95721e11deae560908af71c9cc2c2a9b2db980
Rizhi_KotE <d1mqn1ch@mail.ru>
2019-10-03 10:23:26 +0300

    init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to expression-equal-transformer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/expression_equal_transformer/core_test.clj
@@ -0,0 +1,7 @@
+(ns expression-equal-transformer.core-test
+  (:require [clojure.test :refer :all]
+            [expression-equal-transformer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
